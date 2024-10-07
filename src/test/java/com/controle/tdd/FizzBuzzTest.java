b1d2b1f973f34db378ee518be2337d55fbd63a2b
DucasseRafael <rafael404651@gmail.com>
2024-10-07 09:56:52 +0200

    cycle 1 : FizzBuzz.de(1)  ‘1’

--- src/test/java/com/controle/tdd/FizzBuzzTest.java
+++ src/test/java/com/controle/tdd/FizzBuzzTest.java
@@ -3,9 +3,19 @@ package com.controle.tdd;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+
     @Test
     void fizzBuzz_de_1_devrait_retourner_1() {
-        Assertions.fail("echec cycle 1");
+
+        FizzBuzz fizz = new FizzBuzz();
+        // GIVEN
+        int fizznb = 1;
+        // WHEN
+        String resultat = fizz.de(fizznb);
+        // THEN
+        assertEquals("1", resultat);
     }
 }
